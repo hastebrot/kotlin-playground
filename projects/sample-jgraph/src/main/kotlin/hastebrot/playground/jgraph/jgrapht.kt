d2777d88f49821c0ad198e0dc4f4b0ec3f3f7aaf
Benjamin Gudehus <hastebrot@gmail.com>
2016-05-22 10:19:39 +0200

    (refactor) sample-jgraph: Improve graph examples.

--- projects/sample-jgraph/src/main/kotlin/hastebrot/playground/jgraph/jgrapht.kt
+++ projects/sample-jgraph/src/main/kotlin/hastebrot/playground/jgraph/jgrapht.kt
@@ -4,17 +4,48 @@ import org.jgrapht.graph.DefaultEdge
 import org.jgrapht.graph.SimpleGraph
 
 fun main(args: Array<String>) {
+    val graph0 = buildGraph()
+    println(graph0)
+
+    val graph1 = buildGraphFromAdjacencyLists()
+    println(graph1)
+}
+
+private fun buildGraph(): SimpleGraph<*, *> {
+    val nodes = setOf("a", "b", "c", "d", "e")
+    val edges = setOf(
+        "a" to "b",
+        "a" to "d",
+        "a" to "d",
+        "a" to "e",
+        "b" to "c",
+        "c" to "d"
+    )
+
     val graph = SimpleGraph<String, DefaultEdge>(DefaultEdge::class.java)
+    nodes.forEach { graph.addVertex(it) }
+    edges.forEach { graph.addEdge(it.first, it.second) }
+    return graph
+}
 
-    graph.addVertex("v1")
-    graph.addVertex("v2")
-    graph.addVertex("v3")
-    graph.addVertex("v4")
+private fun buildGraphFromAdjacencyLists(): SimpleGraph<*, *> {
+    val adjacencyLists: Map<String, List<String>> = mapOf(
+        "a" to listOf("b", "d", "d", "e"),
+        "b" to listOf("c", "a"),
+        "c" to listOf("b", "d"),
+        "d" to listOf("a", "a", "c"),
+        "e" to listOf("a")
+    )
 
-    graph.addEdge("v1", "v2")
-    graph.addEdge("v2", "v3")
-    graph.addEdge("v3", "v4")
-    graph.addEdge("v4", "v1")
-
-    println(graph)
+    val graph = SimpleGraph<String, DefaultEdge>(DefaultEdge::class.java)
+    adjacencyLists.forEach {
+        graph.addVertex(it.key)
+    }
+    adjacencyLists.forEach {
+        val (source, targets) = it
+        targets.forEach { target ->
+            graph.addEdge(source, target)
+        }
+    }
+    return graph
 }
