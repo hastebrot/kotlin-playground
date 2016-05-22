ff646db6a00d92a68009138101e6092a780d4161
Benjamin Gudehus <hastebrot@gmail.com>
2016-05-22 15:41:05 +0200

    (feat) sample-jgraph: Add `pairs` extension property.

--- projects/sample-jgraph/src/main/kotlin/hastebrot/playground/jgraph/jgrapht.kt
+++ projects/sample-jgraph/src/main/kotlin/hastebrot/playground/jgraph/jgrapht.kt
@@ -44,8 +44,7 @@ private fun <T> buildGraph(nodes: Set<T>,
     return graph
 }
 
-private fun <T> buildGraph(adjacencyList: Map<T, List<T>>):
-        SimpleGraph<T, DefaultEdge> {
+private fun <T> buildGraph(adjacencyList: Map<T, List<T>>): SimpleGraph<T, DefaultEdge> {
     val graph = SimpleGraph<T, DefaultEdge>(DefaultEdge::class.java)
 
 //    for ((source, targets) in adjacencyList) {
@@ -66,17 +65,18 @@ private fun <T> buildGraph(adjacencyList: Map<T, List<T>>):
         }
     }
 
-//    adjacencyList.map { entry -> entry.key }
-//        .forEach { source -> graph.addVertex(source) }
-//
-//    adjacencyList.flatMap { entry -> entry.value.map { value -> entry.key to value } }
-//        .forEachPair { source, target -> graph.addEdge(source, target) }
+//    adjacencyList.keys.forEach { source -> graph.addVertex(source) }
+//    adjacencyList.pairs.forEachPair { source, target -> graph.addEdge(source, target) }
 
     return graph
 }
 
-private inline fun <S, T> Iterable<Map.Entry<S, T>>.forEachEntry(action: (S, T) -> Unit) =
+private inline fun <K, V> Iterable<Map.Entry<K, V>>.forEachEntry(action: (K, V) -> Unit) =
     forEach { action(it.key, it.value) }
 
-private inline fun <S, T> Iterable<Pair<S, T>>.forEachPair(action: (S, T) -> Unit) =
+private inline fun <A, B> Iterable<Pair<A, B>>.forEachPair(action: (A, B) -> Unit) =
     forEach { action(it.first, it.second) }
+
+private val <K, V> Map<K, List<V>>.pairs: Iterable<Pair<K, V>>
+    get() = flatMap { entry -> entry.value.map { value -> entry.key to value } }
+
