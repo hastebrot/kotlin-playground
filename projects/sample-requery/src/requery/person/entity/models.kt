6e153bd2fe9ab4748c35ec166a98e10a5353ce8d
Benjamin Gudehus <hastebrot@gmail.com>
2016-05-27 23:18:13 +0200

    (feat) sample-requery: Add sample for JPA model.

---
+++ projects/sample-requery/src/requery/person/entity/models.kt
@@ -0,0 +1,138 @@
+package requery.person.entity
+
+import io.requery.Persistable
+import io.requery.query.MutableResult
+import java.io.Serializable
+import java.net.URL
+import java.util.Date
+import java.util.Optional
+import java.util.UUID
+import javax.persistence.CascadeType
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.ForeignKey
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.JoinTable
+import javax.persistence.ManyToMany
+import javax.persistence.ManyToOne
+import javax.persistence.OneToMany
+import javax.persistence.OneToOne
+import javax.persistence.Table
+import javax.persistence.Transient
+import javax.persistence.Version
+
+@Entity
+interface Address : Serializable {
+    @get:Id
+    @get:GeneratedValue
+    var id: Int
+
+    var line1: String
+    var line2: String
+    var state: String
+
+    @get:Column(length = 5)
+    var zip: String
+
+    @get:Column(length = 2)
+    var country: String
+
+    var city: String
+
+    @get:OneToOne(mappedBy = "address")
+    var person: Person
+
+    var type: AddressType
+}
+
+enum class AddressType {
+    HOME,
+    WORK,
+}
+
+@Entity
+@Table(name = "Groups")
+interface Group : Serializable, Persistable {
+    @get:Id
+    @get:GeneratedValue
+    var id: Int
+
+    var name: String
+    val description: Optional<String>
+
+    var type: GroupType
+    var picture: Array<Byte>
+
+    @get:Version
+    var version: Int
+
+    @get:JoinTable(
+        joinColumns = arrayOf(JoinColumn(name = "personId", referencedColumnName = "id")),
+        inverseJoinColumns = arrayOf(JoinColumn(name = "groupId", referencedColumnName = "id"))
+    )
+    @get:ManyToMany
+    var persons: MutableResult<Person>
+
+    @get:Transient
+    var temporaryName: String
+}
+
+enum class GroupType {
+    PUBLIC,
+    PRIVATE,
+}
+
+@Entity
+interface Person : Serializable {
+    @get:Id
+    @get:GeneratedValue
+    var id: Int
+
+    var name: String
+    var email: String
+    var birthday: Date
+
+    @get:Column(nullable = true)
+    var age: Int
+
+    @get:OneToOne
+    @get:JoinColumn(foreignKey = ForeignKey())
+    var address: Address
+
+    @get:OneToMany(
+        mappedBy = "owner",
+        cascade = arrayOf(CascadeType.REMOVE, CascadeType.PERSIST)
+    )
+    var phoneNumbers: MutableResult<Phone>
+
+    @get:OneToMany(mappedBy = "owner")
+    var phoneNumbersSet: MutableSet<Phone>
+
+    @get:OneToMany
+    var phoneNumbersList: MutableList<Phone>
+
+    @get:ManyToMany(mappedBy = "persons")
+    var groups: MutableResult<Group>
+
+    var about: String
+
+    @get:Column(unique = true)
+    var uuid: UUID
+
+    var homepage: URL
+}
+
+@Entity
+interface Phone : Serializable {
+    @get:Id
+    @get:GeneratedValue
+    var id: Int
+
+    var phoneNumber: String
+    var normalized: Boolean
+
+    @get:ManyToOne
+    var owner: Person
+}
---
+++ projects/sample-requery/src/requery/person/sample.kt
@@ -0,0 +1,146 @@
+package requery.person
+
+import com.zaxxer.hikari.HikariDataSource
+import io.requery.Persistable
+import io.requery.cache.EntityCacheBuilder
+import io.requery.meta.EntityModel
+import io.requery.sql.BoundParameters
+import io.requery.sql.Configuration
+import io.requery.sql.ConfigurationBuilder
+import io.requery.sql.EntityDataStore
+import io.requery.sql.SchemaModifier
+import io.requery.sql.StatementListener
+import io.requery.sql.TableCreationMode
+import org.apache.logging.log4j.Level
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.core.LoggerContext
+import org.apache.logging.log4j.core.appender.ConsoleAppender
+import org.apache.logging.log4j.core.layout.PatternLayout
+import requery.person.entity.AddressEntity
+import requery.person.entity.Models
+import requery.person.entity.PersonEntity
+import rx.schedulers.Schedulers
+import java.sql.Statement
+import javax.cache.Caching
+import javax.sql.DataSource
+
+fun main(args: Array<String>) {
+    configureRootLogger(Level.OFF)
+
+    val dataSource = buildDataSource()
+    val entityModel = Models.ENTITY
+
+    val dataStoreConfig = buildDataStoreConfig(dataSource, entityModel)
+    val dataStore = EntityDataStore<Persistable>(dataStoreConfig)
+
+    createTables(dataStoreConfig)
+    runSamples(dataStore)
+
+    dataStore.close()
+}
+
+private fun runSamples(dataStore: EntityDataStore<Persistable>) {
+    dataStore.insert(PersonEntity().apply {
+        age = 30
+        address = AddressEntity().apply {
+            city = "hamburg"
+        }
+    })
+    dataStore.insert(PersonEntity().apply {
+        age = 31
+    })
+
+    val scalar = dataStore.count(PersonEntity::class.java)
+        .get()
+    scalar.value().let { println(it) }
+
+    val result = dataStore.select(PersonEntity::class.java)
+        .limit(10)
+        .get()
+    result.forEach { println(it) }
+
+    val countScalar = dataStore.count(PersonEntity::class.java)
+        .get()
+    countScalar.toSingle()
+        .subscribe { println(it) }
+
+    val selectResult = dataStore.select(PersonEntity::class.java)
+        .get()
+    selectResult.toObservable()
+        .subscribeOn(Schedulers.io())
+        .subscribe { println(it) }
+
+    val selectResult2 = dataStore.select(PersonEntity::class.java)
+        .get()
+    selectResult2.toObservable()
+        .subscribe {
+            println(PersonEntity.AGE.property().get(it))
+        }
+
+    Models.ENTITY
+        .typeOf(PersonEntity::class.java)
+        .attributes()
+        .associateBy { it.name() }
+        .let { println(it) }
+}
+
+private fun buildDataStoreConfig(dataSource: DataSource,
+                                 entityModel: EntityModel): Configuration {
+    val cacheManager = Caching.getCachingProvider().cacheManager
+    val configuration = ConfigurationBuilder(dataSource, entityModel)
+//        .useDefaultLogging()
+        .addStatementListener(sqlStatementListener)
+        .setEntityCache(EntityCacheBuilder(entityModel)
+            .useReferenceCache(true)
+            .useSerializableCache(true)
+            .useCacheManager(cacheManager)
+            .build())
+        .build()
+    return configuration
+}
+
+private fun createTables(configuration: Configuration) {
+    val schemaModifier = SchemaModifier(configuration)
+    schemaModifier.createTables(TableCreationMode.DROP_CREATE)
+}
+
+private fun buildDataSource(): DataSource {
+    val dataSource = HikariDataSource()
+    dataSource.jdbcUrl = "jdbc:h2:mem:samples;DB_CLOSE_DELAY=-1"
+    dataSource.username = "sa"
+    return dataSource
+}
+
+private fun configureRootLogger(loggerLevel: Level) {
+    val loggerContext = LogManager.getContext(false) as LoggerContext
+    val loggerName = LogManager.ROOT_LOGGER_NAME
+
+    val consoleAppender = ConsoleAppender
+        .createDefaultAppenderForLayout(PatternLayout.createDefaultLayout())
+        .apply { start() }
+
+    loggerContext.configuration
+        .getLoggerConfig(loggerName)
+        .apply {
+            level = loggerLevel
+            addAppender(consoleAppender, null, null)
+        }
+}
+
+private val sqlStatementListener = object : StatementListener {
+    override fun beforeExecuteQuery(statement: Statement,
+                                    sql: String,
+                                    parameters: BoundParameters?) {
+        println(sql)
+    }
+
+    override fun afterExecuteQuery(statement: Statement) { }
+
+    override fun beforeExecuteUpdate(statement: Statement,
+                                     sql: String,
+                                     parameters: BoundParameters?) {
+        println(sql)
+    }
+
+    override fun afterExecuteUpdate(statement: Statement) { }
+}
